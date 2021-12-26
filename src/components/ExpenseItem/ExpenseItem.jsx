4783c57f0463475f2436a197b8f27ea1329e3363
KMihajlo <mihajlo.kragujevski@yahoo.com>
2021-12-26 20:03:31 +0100

    Changing the title by using state

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const changeTitleHandler = () => {
+    setTitle("Updated!");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={changeTitleHandler}>Change Title</button>
     </Card>
   );
 }
