3b168ebe14762920ced265241d94303460b6c7e8
KMihajlo <mihajlo.kragujevski@yahoo.com>
2021-12-27 01:51:58 +0100

    Established the UI of the filtering of expenses

--- src/components/ExpenseItem/ExpenseItem.jsx
+++ src/components/ExpenseItem/ExpenseItem.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "../Card/Card";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const changeTitleHandler = () => {
-    setTitle("Updated!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={changeTitleHandler}>Change Title</button>
     </Card>
   );
 };
