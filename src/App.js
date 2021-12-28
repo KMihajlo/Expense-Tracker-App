0f0ec74fe118d701cf77108bbd0b72923cdf932f
KMihajlo <mihajlo.kragujevski@yahoo.com>
2021-12-28 16:33:37 +0100

    Made the list to be rendered dynamically

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const addExpenseHandler = (data) => {
-    console.log(data)
-  };
+const INITIAL_EXPENSES = [
+  {
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
-  const expenses = [
-    {
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-    },
-    {
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const App = () => {
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+
+  const addExpenseHandler = (data) => {
+    // Special way of assigning data to state dependant on the previous state
+    setExpenses((previous) => {
+      return [data, ...previous];
+    });
+  };
 
   return (
     <div>
